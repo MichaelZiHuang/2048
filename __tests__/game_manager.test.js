574332daf144ed5e26182a6c15c466b427d25001
Michael Huang <sleep3030@gmail.com>
2019-03-05 20:37:59 -0800

    Finishing game_manager tests

--- __tests__/game_manager.test.js
+++ __tests__/game_manager.test.js
@@ -17,6 +17,30 @@ $(document.body).append(
 
 const testGame = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 
-test('game initialization', () => {
-    
-});
+//test('Game initialization', () => {
+test('Ensure 2 Tiles during startup', () => {
+	expect(testGame.startTiles).toBe(2);
+})
+
+test('Fresh Process, Ensure Fresh State', () => {
+	expect(testGame.score).toBe(0);
+	expect(testGame.over).toBe(false);
+	expect(testGame.won).toBe(false);
+	expect(testGame.keepPlaying).toBe(false);
+	expect(testGame.startTiles).toBe(2);
+})
+
+test('Default Grid Size: 4', () => {
+	expect(testGame.size).toBe(4);
+})
+
+test('Test Terminated Game Function', () => {
+	testGame.over = true;
+	expect(testGame.isGameTerminated()).toBe(true);
+	testGame.over = false;
+	testGame.won = false;
+	expect(testGame.isGameTerminated()).toBe(false);
+})
+
+
+//});
